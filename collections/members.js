38d2192c360f23f726c4fd3aa59b1efa9fe0d5e2
James Michael Reilly <jmr8893@rit.edu>
2015-10-01 20:50:45 -0400

    Added Schema for Member Collection

---
+++ collections/members.js
@@ -0,0 +1,64 @@
+Members = new Mongo.collection('members');
+Contacts.attachSchema(new SimpleSchema({
+  firstName: {
+    type: String,
+    label: "FirstName",
+  },
+  lastName: {
+    type: String,
+    label: "Last Name"
+  },
+  email: {
+    type: String,
+    label: "Email"
+    autoform: {
+      omit: true
+    },
+    autoValue: function() {
+      if (this.isInsert) {
+        var username = this.field("username");
+        if (username.isSet) {
+          return content.value + "@rit.edu";
+        } else {
+          this.unset();
+        }
+      }
+    }
+  },
+  username: {
+    type: String,
+    label: "Username"
+  },
+  uID: {
+    type: Number,
+    label: "University ID #",
+    max: 9
+  },
+  swipeAccess: {
+    type: Boolean,
+    label: "Swipe",
+    autoform: {
+      omit: true
+    },
+    autoValue: function() {
+      if (this.isInsert) {
+        return false;
+      }
+    }
+  },
+  createdAt: {
+    type: Date,
+    autoform: {
+      omit: true
+    },
+    autoValue: function() {
+      if (this.isInsert) {
+        return new Date;
+      } else if (this.isUpsert) {
+        return {$setOnInsert: new Date};
+      } else {
+        this.unset();
+      }
+    }
+  }
+}));
